9933cdbbb96ce1ff8fd550373fbbfebf47d8abd6
J Linux <robertsj3@appstate.edu>
2022-06-06 16:09:59 -0400

    Start work on gaussian process model

--- source/mycoGaussModel.py
+++ source/mycoGaussModel.py
@@ -1,17 +1,50 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import pandas as pd
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
+import matplotlib.pyplot as plt
 
 whichMold ="my1"
+removeDups = True
 dataset = np.loadtxt(f"data/{whichMold}yielddata.csv", delimiter=',')
+incubation_days = dataset[:,1].tolist()
+yield_g = dataset[:,4].tolist()
 
-inc = dataset[:,1]
-sed = dataset[:,2]
-plat = dataset[:,3]
-yld = dataset[:,4]
+dups=[idx for idx, item in enumerate(incubation_days) if item in incubation_days[:idx]]
+for d in dups:
+    incubation_days[d] = -1
+    yield_g[d] = -1
 
-kernel = 1 * RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e2))
-gaussian_process = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
-gaussian_process.fit(inc, yld)
+if removeDups:
+    incubation_days = [value for value in incubation_days if value != -1]
+    yield_g = [value for value in yield_g if value != -1]
+    
+incubation_days, yield_g = zip(*sorted(zip(incubation_days,yield_g),key=lambda x: x[0]))
+
+X = np.asarray(incubation_days).reshape(-1,1)
+y = np.asarray(yield_g)
+
+rng = np.random.RandomState(1)
+training_indices = rng.choice(np.arange(y.size), size=15, replace=False)
+X_train, y_train = X[training_indices], y[training_indices]
+kernel = 1 * RBF(length_scale=2, length_scale_bounds=(2.0, 2.5)) + WhiteKernel(noise_level=0.01)
+gaussian_process = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10000)
+gaussian_process.fit(X_train, y_train)
+gaussian_process.kernel_
+mean_prediction, std_prediction = gaussian_process.predict(X, return_std=True)
+
+plt.plot(X, y, label=r"Linear connection between points", linestyle="dotted")
+plt.scatter(X_train, y_train, label="Observations")
+plt.plot(X, mean_prediction, label="Mean prediction")
+plt.fill_between(
+    X.ravel(),
+    mean_prediction - 1.96 * std_prediction,
+    mean_prediction + 1.96 * std_prediction,
+    alpha=0.5,
+    label=r"95% confidence interval",
+)
+plt.legend()
+plt.xlabel("Incubation days")
+plt.ylabel("Yield (g/L)")
+_ = plt.title("Gaussian process regression on noisy incubation days data with duplicates removed")
+plt.show()
